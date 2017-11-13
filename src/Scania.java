4e99854fea5c11cc7e770d6795a6f875c65aa152
Johannes Gustavsson <Jogust95gmail.com>
2017-11-13 12:32:50 +0100

    Added the methods lowerTipper and raiseTipper. Also javadoc for the methods.

--- src/Scania.java
+++ src/Scania.java
@@ -10,14 +10,31 @@ public class Scania extends Car {
         super(2, Color.black, 550, "Scania Truck");
         turboOn = false;
         turboPower = 1.5;
-        stopEngine;
+        tipAngle = 0;
+        stopEngine();
     }
 
     public double speedFactor() {
         double turbo = 1;
         if(turboOn) turbo = turboPower;
-        return getEnginePower() * 0.001 * turboPower;
+        return getEnginePower() * 0.001 * turbo;
     }
+
+    /**
+     * Increases tipAngle but only if lower then 70.
+     */
+    private void raiseTipper(){
+        if(tipAngle < 70 ){tipAngle++;}
+    }
+
+    /**
+     * Decreases int tipAngle, but only if it higher then 0.
+     */
+    private void lowerTipper(){
+        if(tipAngle > 0)tipAngle--;
+    }
+
+
     public void setTurboOn() {
         turboOn = true;
     }
