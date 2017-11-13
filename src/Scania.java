d4b23b2b4a2694ac990ac39182f10147a18b5f82
jacobsqy <jacobsqy@zenbook.it>
2017-11-13 12:10:09 +0100

    HP and Turbo

--- src/Scania.java
+++ src/Scania.java
@@ -2,15 +2,26 @@ import java.awt.*;
 
 public class Scania extends Car {
     private int tipAngle;
+    private boolean turboOn;
+    private double turboPower;
 
 
     public Scania (){
-        super(2, Color.black, 150, "Scania Truck");
+        super(2, Color.black, 550, "Scania Truck");
+        turboOn = false;
+        turboPower = 1.5;
+        stopEngine;
     }
 
     public double speedFactor() {
-        return getEnginePower() * 0.01;
+        double turbo = 1;
+        if(turboOn) turbo = turboPower;
+        return getEnginePower() * 0.001 * turboPower;
     }
-
-
+    public void setTurboOn() {
+        turboOn = true;
+    }
+    public void setTurboOff() {
+        turboOn = false;
+    }
 }
