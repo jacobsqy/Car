37964b9f43c04789a65c41f58d6855a511283dc1
Johannes Gustavsson <Jogust95gmail.com>
2017-11-14 08:24:03 +0100

    Added TransportTruck Class with Construktor() and Speedfactor()

---
+++ src/TransportTruck.java
@@ -0,0 +1,13 @@
+public class TransportTruck extends Car {
+
+
+    public TransportTruck(){
+        super();
+    }
+
+
+    @Override
+    public double speedFactor() {
+        return 0;
+    }
+}
