af9b8c53d0bcf0aca850693e92fba19a2b5147b2
Johannes Gustavsson <Jogust95gmail.com>
2017-11-14 08:36:45 +0100

    Added the classes Vehicle and Truck

--- src/CarTransport.java
+++ src/CarTransport.java
@@ -4,9 +4,11 @@ public class CarTransport extends Car {
 
     private boolean turboOn;
     private double turboPower;
+    private int tipAngle;
 
     public CarTransport(){
         super(2, Color.black, 550, "cartransport");
+        tipAngle = 0;
     }
 
 
@@ -15,5 +17,11 @@ public class CarTransport extends Car {
         double turbo = 1;
         if(turboOn) turbo = turboPower;
         return getEnginePower() * 0.001 * turbo;
+    }
+    @Override
+    public void move() {
+        if (tipAngle != 0) {
+            super.move();
+        }
     }
 }
