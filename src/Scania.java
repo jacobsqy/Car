2bcd2cb2fc58593b951552fb64c0d1a5f08fea0a
jacobsqy <jacobsqy@zenbook.it>
2017-11-14 08:46:24 +0100

    moved move() to Trucks

--- src/Scania.java
+++ src/Scania.java
@@ -42,12 +42,6 @@ public class Scania extends Car {
      * This method adds currentSpeed() to the y position to make it go forward,
      * but only if tipAngle != 0.
      */
-    @Override
-    public void move() {
-      if (tipAngle != 0) {
-        super.move();
-      }
-    }
 
     public void setTurboOn() {
         turboOn = true;
--- src/Trucks.java
+++ src/Trucks.java
@@ -1,3 +1,10 @@
-public abstract class Trucks {
+public abstract class Trucks extends Car {
 
+  
+  @Override
+  public void move() {
+    if (tipAngle != 0) {
+      super.move();
+    }
+  }
 }
