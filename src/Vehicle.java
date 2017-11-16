7f03169dcf9d85b6fd4ce29e796e67840ed65534
jacobsqy <jacobsqy@zenbook.it>
2017-11-16 14:27:27 +0100

    fixed currentDirIndex

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -8,11 +8,13 @@ public abstract class Vehicle {
     LEFT
   }
   private Dir[] dirs = {Dir.FORWARD, Dir.RIGHT, Dir.BACK, Dir.LEFT};
-  private Dir currentDirIndex = 0;
+  private int currentDirIndex = 0;
   private Dir dir;
 
   private void move(double x, double y) {
-    
+    switch {
+      case
+    }
   }
 
   /**
