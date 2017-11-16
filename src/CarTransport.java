3fcc286223e6359c5ea63d7896e8115ba44230c1
Johannes Gustavsson <Jogust95gmail.com>
2017-11-16 16:06:39 +0100

    moved WithinLoadingRange()

--- src/CarTransport.java
+++ src/CarTransport.java
@@ -13,15 +13,6 @@ public class CarTransport extends Truck {
         loadingRange = 2.0;
     }
 
-
-    private boolean withinLoadingRange(Car car) {
-      double dy = car.getyPos() - getyPos();
-      double dx = car.getxPos() - getxPos();
-      double distance = Math.sqrt(Math.pow(dy, 2) + Math.pow(dx, 2));
-      return loadingRange > distance;
-    }
-
-
     @Override
     public void move() {
         if (rampState == rampState.UP) {
--- src/TransportVehicle.java
+++ src/TransportVehicle.java
@@ -11,8 +11,18 @@ public abstract class TransportVehicle extends Vehicle {
     }
 
     public TransportVehicle(double enginePower, Color color, String modelname){
-        super(enginePower, color, modelname)
+        super(enginePower, color, modelname);
+        ramp = new Stack<>();
+        loadingRange = 2.0;
+        rampState = rampstate.UP;
     }
+    private boolean withinLoadingRange(Car car) {
+        double dy = car.getyPos() - getyPos();
+        double dx = car.getxPos() - getxPos();
+        double distance = Math.sqrt(Math.pow(dy, 2) + Math.pow(dx, 2));
+        return loadingRange > distance;
+    }
+
 
 
     private void load(Car car) {
