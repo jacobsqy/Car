77770c2d03fd5951d978f03b4ee43149eb1efa3a
Johannes Gustavsson <Jogust95gmail.com>
2017-11-17 10:08:10 +0100

    Made the tests startEngine(), stopEngine() and isStill()

--- src/CarTransport.java
+++ src/CarTransport.java
@@ -1,5 +1,4 @@
 import java.awt.*;
-import java.util.Stack;
 
 public class CarTransport extends Truck {
 
--- tests/VehicleTest.java
+++ tests/VehicleTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
@@ -39,14 +40,49 @@ class VehicleTest {
 
     @Test
     void startEngine() {
+        Car car1 = new Volvo240();
+        car1.startEngine();
+        double output = car1.getCurrentSpeed();
+        assertEquals(0.1, output);
+
+        Vehicle truck = new Scania();
+        truck.startEngine();
+        double output2 = truck.getCurrentSpeed();
+        assertEquals(0.1, output2);
+
+        Vehicle ferry = new Ferry();
+        ferry.startEngine();
+        double output3 = ferry.getCurrentSpeed();
+        assertEquals(0.1, output3);
     }
 
     @Test
     void stopEngine() {
+        Vehicle car1 = new Saab95();
+        car1.stopEngine();
+        double output = car1.getCurrentSpeed();
+        assertEquals(0, output);
+
+        Vehicle truck1 = new Scania();
+        truck1.stopEngine();
+        double output2 = truck1.getCurrentSpeed();
+        assertEquals(0, output2);
+
+        Vehicle ferry = new Ferry();
+        ferry.stopEngine();
+        double output3 = ferry.getCurrentSpeed();
+        assertEquals(0, output3);
     }
 
     @Test
     void isStill() {
+        Car car1 = new Volvo240();
+        car1.startEngine();
+        car1.gas(0.9);
+        out.println(car1.getCurrentSpeed());
+        assertFalse(car1.isStill());
+        car1.stopEngine();
+        assertTrue(car1.isStill());
     }
 
     @Test
